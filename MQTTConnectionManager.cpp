1a173f5a2bb650823a178a0e4bb44540ca7b5d03
admin <admin@DESKTOP-L7RGLLV>
2026-01-08 22:58:45 +0300

    Добавьте файлы проекта.

---
+++ MQTTConnectionManager.cpp
@@ -0,0 +1,52 @@
+﻿#include "MQTTConnectionManager.h"
+
+#include <QLabel>
+#include <QGridLayout>
+
+MQTTConnectionManager::MQTTConnectionManager(QWidget* parent) : QWidget(parent) {
+	m_pmqttClient = new QMqttClient(this);
+
+	QLabel* lblHost = new QLabel("Host", this);
+	QLabel* lblPort = new QLabel("Port", this);
+	m_pleHost = new QLineEdit("test.mosquitto.org",this);
+	m_plePort = new QLineEdit("1883",this);
+	m_pbtnConnect = new QPushButton("Connect", this);
+
+	QGridLayout* mainLayout = new QGridLayout(this);
+
+	mainLayout->addWidget(lblHost, 0, 0, Qt::AlignRight);
+	mainLayout->addWidget(m_pleHost, 0, 1);
+	mainLayout->addWidget(lblPort, 1, 0, Qt::AlignRight);
+	mainLayout->addWidget(m_plePort, 1, 1);
+	mainLayout->addWidget(m_pbtnConnect, 2, 0, 1, 2);
+
+	connect(m_pbtnConnect, &QPushButton::clicked, this, &MQTTConnectionManager::onBtnConnectClick);
+}
+
+void MQTTConnectionManager::onBtnConnectClick() {
+	m_pmqttClient->setHostname(m_pleHost->text());
+	m_pmqttClient->setPort(static_cast<qint16>(m_plePort->text().toInt()));
+
+	if (m_pmqttClient->state() == QMqttClient::Disconnected) {
+		m_pleHost->setEnabled(false);
+		m_plePort->setEnabled(false);
+
+		m_pbtnConnect->setText("Disconnect");
+		m_pmqttClient->connectToHost();
+
+		if (m_pmqttClient->state() == QMqttClient::Disconnected) {
+			m_pleHost->setEnabled(true);
+			m_plePort->setEnabled(true);
+
+			m_pbtnConnect->setText("Connect");
+			m_pmqttClient->disconnectFromHost();
+		}
+	}
+	else {
+		m_pleHost->setEnabled(true);
+		m_plePort->setEnabled(true);
+
+		m_pbtnConnect->setText("Connect");
+		m_pmqttClient->disconnectFromHost();
+	}
+}
---
+++ MQTTConnectionManager.h
@@ -0,0 +1,23 @@
+﻿#ifndef MQTT_CONNECTION_MANAGER_H
+#define MQTT_CONNECTION_MANAGER_H
+
+#include <QWidget>
+#include <QMqttClient>
+#include <QLineEdit>
+#include <QPushButton>
+
+class MQTTConnectionManager : public QWidget {
+	Q_OBJECT
+public:
+	MQTTConnectionManager(QWidget* parent = nullptr);
+private:
+	QMqttClient* m_pmqttClient = nullptr;
+
+	QLineEdit* m_pleHost = nullptr;
+	QLineEdit* m_plePort = nullptr;
+	QPushButton* m_pbtnConnect = nullptr;
+public slots:
+	void onBtnConnectClick();
+};
+
+#endif MQTT_CONNECTION_MANAGER_H	
